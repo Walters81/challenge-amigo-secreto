0d6e7343c9b0c9359ee6155144b323ea2804cca7
Walter Salcedo <salcedowa@gmail.com>
2025-02-23 22:13:05 -0500

    Se agregó la función agregarAmigo en el app

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
 
-function name(params) {
+let listaDeAmigos = [];
+let listaDeNombres = document.getElementById("listaAmigos");
+let elementoAmigoSorteado = document.getElementById("resultado");
+
+
+//agregar los nombres de los amigos
+function agregarAmigo() {
+    let nombreDeAmigos = document.getElementById('amigo').value;
+
+    if(nombreDeAmigos===""){
+        alert("Ingrese un nombre válido");
+    }else{
+        listaDeAmigos.push[nombreDeAmigos];
+        let nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = nombreDeAmigos;
+        listaDeNombres.appendChild(nuevoElemento);
+        console.log(nombreDeAmigos);
+
+        limpiarCaja();
+    }
+}
     
+//Sortear el amigo secreto
+function sortearAmigo() {
+    if(listaDeAmigos.length > 0) {
+        let amigoSorteado = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+        elementoAmigoSorteado.textContent = `El amigo secreto es: ${amigoSorteado}`;
+        return amigoSorteado;
+    }else{
+        alert("Debes ingresar al menos un nombre para poder sortear");
+        return null;
+    }
+}
+sortearAmigo();
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
 }
